efc9a2b7bfaabe88e4e7f63bef1acc7c49f84993
Skythrew <mael.guerin@outlook.fr>
2023-01-31 19:24:36 +0100

    feat: add setup command, --help and --use-chroot option

--- evobld.sh
+++ evobld.sh
@@ -10,20 +10,104 @@
 
 # We begin by creating the chroot directory if it does not already exist.
 # If it exists, we delete it, we ask the user if he wants to use the existing chroot directory.
+ROOT=$PWD/chroot
+
+install_toolchain() {
+    ROOT=$ROOT evox sync
+    ROOT=$ROOT evox get glibc gcc m4 ncurses bash coreutils diffutils file findutils gawk grep gzip make patch sed tar xz binutils texinfo meson ninja gettext bison perl python util-linux kernel-headers libtool automake autoconf pkg-config evox evoke -y
+}
+
+setup_chroot() {
+    mkdir -p $ROOT
+    mkdir -p $ROOT/dev/pts
+    mkdir -p $ROOT/proc
+    mkdir -p $ROOT/sys
+    mkdir -p $ROOT/run
+    mkdir -p $ROOT/tmp
+    mkdir -p $ROOT/etc
+    mkdir -p $ROOT/var
+    mkdir -p $ROOT/usr/bin
+    mkdir -p $ROOT/usr/sbin
+    mkdir -p $ROOT/usr/lib
+    mkdir -p $ROOT/usr/share
+    mkdir -p $ROOT/usr/include
+    mkdir -p $ROOT/usr/libexec
+    mkdir -p $ROOT/boot
+    mkdir -p $ROOT/mnt
+
+    ln -s usr/bin $ROOT/bin
+    ln -s usr/lib $ROOT/lib
+    ln -s usr/sbin $ROOT/sbin
+    mkdir -p $ROOT/lib64
+    mkdir -p $ROOT/usr/lib32
+    ln -s usr/lib32 $ROOT/lib32
+
+    mkdir -p $ROOT/{home,mnt,opt,srv}
+    mkdir -p $ROOT/etc/{opt,sysconfig}
+    mkdir -p $ROOT/lib/firmware
+    mkdir -p $ROOT/media/{floppy,cdrom}
+    mkdir -p $ROOT/usr/{,local/}{include,src}
+    mkdir -p $ROOT/usr/local/{bin,lib,sbin}
+    mkdir -p $ROOT/usr/{,local/}share/{color,dict,doc,info,locale,man}
+    mkdir -p $ROOT/usr/{,local/}share/{misc,terminfo,zoneinfo}
+    mkdir -p $ROOT/usr/{,local/}share/man/man{1..8}
+    mkdir -p $ROOT/var/{cache,local,log,mail,opt,spool}
+    mkdir -p $ROOT/var/lib/{color,misc,locate}
+    cp /etc/resolv.conf $ROOT/etc/resolv.conf
+    cp /etc/hosts $ROOT/etc/hosts
+    cp /etc/evox.conf $ROOT/etc/evox.conf
+    ROOT=$ROOT evox init
+    install_toolchain
+    ln -s bash $ROOT/bin/sh
+}
+
+print_help() {
+    echo "Usage: evobld.sh [--use-chroot <chroot directory>]"
+    echo "                 setup"
+}
+
+if [ "$1" = "--help" ]; then
+    print_help
+    exit 0
+fi
+
+if [ "$1" = "--use-chroot" ] && [ "$2" = "" ]; then
+    print_help
+    exit 1
+fi
+
+if [ "$1" != "--use-chroot" ] && [ "$1" != "setup" ] && [ "$1" != "" ]; then
+    print_help
+    exit 1
+fi
+
+if [ "$1" == "setup" ]; then
+    setup_chroot
+    exit 0
+fi
+
 ALREADY_EXISTS=0
 
-if [ -d chroot ]; then
+# If there is the --use-chroot option, we use the existing chroot directory given as argument.
+if [ "$1" = "--use-chroot" ]; then
+    if [ -d $2 ]; then
+        ROOT=$2
+        ALREADY_EXISTS=1
+    else
+        echo "The chroot directory does not exist."
+        exit 1
+    fi
+fi
+
+if [ -d $ROOT ]; then
     echo "The chroot directory already exists."
     echo "Do you want to delete it and create a new one? (y/n)"
     read answer
     if [ "$answer" = "y" ]; then
-        rm -rf chroot
-        mkdir chroot
+        rm -rf $ROOT
     else
         ALREADY_EXISTS=1
     fi
-else
-    mkdir chroot
 fi
 
 # If the chroot directory does not already exist, we must run evox to create the toolchain.
@@ -31,100 +115,57 @@ fi
 
 # We can now install the toolchain.
 if [ $ALREADY_EXISTS -eq 0 ]; then
-    mkdir -p chroot/dev/pts
-    mkdir -p chroot/proc
-    mkdir -p chroot/sys
-    mkdir -p chroot/run
-    mkdir -p chroot/tmp
-    mkdir -p chroot/etc
-    mkdir -p chroot/var
-    mkdir -p chroot/usr/bin
-    mkdir -p chroot/usr/sbin
-    mkdir -p chroot/usr/lib
-    mkdir -p chroot/usr/share
-    mkdir -p chroot/usr/include
-    mkdir -p chroot/usr/libexec
-    mkdir -p chroot/boot
-    mkdir -p chroot/mnt
-
-    ln -s usr/bin chroot/bin
-    ln -s usr/lib chroot/lib
-    ln -s usr/sbin chroot/sbin
-    mkdir -p chroot/lib64
-    mkdir -p chroot/usr/lib32
-    ln -s usr/lib32 chroot/lib32
-
-    mkdir -p chroot/{home,mnt,opt,srv}
-    mkdir -p chroot/etc/{opt,sysconfig}
-    mkdir -p chroot/lib/firmware
-    mkdir -p chroot/media/{floppy,cdrom}
-    mkdir -p chroot/usr/{,local/}{include,src}
-    mkdir -p chroot/usr/local/{bin,lib,sbin}
-    mkdir -p chroot/usr/{,local/}share/{color,dict,doc,info,locale,man}
-    mkdir -p chroot/usr/{,local/}share/{misc,terminfo,zoneinfo}
-    mkdir -p chroot/usr/{,local/}share/man/man{1..8}
-    mkdir -p chroot/var/{cache,local,log,mail,opt,spool}
-    mkdir -p chroot/var/lib/{color,misc,locate}
-    cp /etc/resolv.conf chroot/etc/resolv.conf
-    cp /etc/hosts chroot/etc/hosts
-    cp /etc/evox.conf chroot/etc/evox.conf
-    ROOT=$PWD/chroot evox init
-    ROOT=$PWD/chroot evox sync
-    ROOT=$PWD/chroot evox get glibc gcc m4 ncurses bash coreutils diffutils file findutils gawk grep gzip make patch sed tar xz binutils texinfo meson ninja gettext bison perl python util-linux kernel-headers libtool automake autoconf pkg-config evox evoke -y
-    ln -s bash chroot/bin/sh
-    # We chroot into the environment and install some python packages.
-    chroot chroot /bin/bash -c "pip3 install -r /usr/lib/evox/requirements.txt"
-    chroot chroot /bin/bash -c "pip3 install -r /usr/lib/evoke/requirements.txt"
-else
-    ROOT=$PWD/chroot evox sync
-    ROOT=$PWD/chroot evox get glibc gcc m4 ncurses bash coreutils diffutils file findutils gawk grep gzip make patch sed tar xz binutils texinfo meson ninja gettext bison perl python util-linux kernel-headers libtool automake autoconf pkg-config evox evoke -y
-    chroot chroot /bin/bash -c "pip3 install -r /usr/lib/evox/requirements.txt"
-    chroot chroot /bin/bash -c "pip3 install -r /usr/lib/evoke/requirements.txt"
+    setup_chroot
 fi
+
+install_toolchain
+
+chroot $ROOT /bin/bash -c "pip3 install -r /usr/lib/evox/requirements.txt"
+chroot $ROOT /bin/bash -c "pip3 install -r /usr/lib/evoke/requirements.txt"
 
 # We can now build the evx package.
 # We need to copy the package folder to the chroot directory.
 # So we copy all the files (except the chroot directory) in the current directory to the chroot directory.
-if [ -d chroot/usr/src/$(basename $PWD) ]; then
-    rm -rf chroot/usr/src/$(basename $PWD)
+if [ -d $ROOT/usr/src/$(basename $PWD) ]; then
+    rm -rf $ROOT/usr/src/$(basename $PWD)
 fi
 
-mkdir -p chroot/usr/src/$(basename $PWD)
-cp -r metadata scripts chroot/usr/src/$(basename $PWD)
+mkdir -p $ROOT/usr/src/$(basename $PWD)
+cp -r metadata scripts $ROOT/usr/src/$(basename $PWD)
 
 # We mount the necessary directories in the chroot environment.
-mount -o bind /dev chroot/dev
-mount -o bind /dev/pts chroot/dev/pts
-mount -o bind /proc chroot/proc
-mount -o bind /sys chroot/sys
-mount -o bind /run chroot/run
-mount -o bind /tmp chroot/tmp
+mount -o bind /dev $ROOT/dev
+mount -o bind /dev/pts $ROOT/dev/pts
+mount -o bind /proc $ROOT/proc
+mount -o bind /sys $ROOT/sys
+mount -o bind /run $ROOT/run
+mount -o bind /tmp $ROOT/tmp
 
 # We chroot into the environment and build the evx package.
 echo "Going to chroot into the environment."
 # chroot chroot /bin/bash -c "cd /usr/src/$(basename $PWD) && JOBS=$JOBS evoke build"
 # The above command does not work (it executes the evoke command multiple times).
 # So we use the following command instead.
-chroot chroot /bin/bash << EOF
+chroot $ROOT /bin/bash << EOF
 cd /usr/src/$(basename $PWD)
 JOBS=$JOBS evoke build
 EOF
 
 # We unmount the directories.
-umount chroot/dev/pts
-umount chroot/dev
-umount chroot/proc
-umount chroot/sys
-umount chroot/run
-umount chroot/tmp
+umount $ROOT/dev/pts
+umount $ROOT/dev
+umount $ROOT/proc
+umount $ROOT/sys
+umount $ROOT/run
+umount $ROOT/tmp
 
 # We copy the evx package to the current directory.
-cp chroot/usr/src/$(basename $PWD)-*.evx .
+cp $ROOT/usr/src/$(basename $PWD)-*.evx .
 # If they exist, we copy the log files to the current directory.
-if [ -f chroot/usr/src/$(basename $PWD)/build.stdout.log ]; then
-    cp chroot/usr/src/$(basename $PWD)/build.stdout.log .
+if [ -f $ROOT/usr/src/$(basename $PWD)/build.stdout.log ]; then
+    cp $ROOT/usr/src/$(basename $PWD)/build.stdout.log .
 fi
 
-if [ -f chroot/usr/src/$(basename $PWD)/build.stderr.log ]; then
-    cp chroot/usr/src/$(basename $PWD)/build.stderr.log .
+if [ -f $ROOT/usr/src/$(basename $PWD)/build.stderr.log ]; then
+    cp $ROOT/usr/src/$(basename $PWD)/build.stderr.log .
 fi
